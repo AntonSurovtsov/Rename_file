cd6bb777842addcbf395499b6dee66fc7602a3c0
Антон Суровцов <asurovcov@outlook.com>
2022-04-06 20:49:11 +0300

    Add class Symbols

--- Rename_file/Program.cs
+++ Rename_file/Program.cs
@@ -4,12 +4,12 @@ using System.Windows.Forms;
 
 namespace Rename_file
 {
-    internal class Program
+    public class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            var openFileDialog = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
 
             Console.Write("Нажмите Enter, чтобы продолжить...");
@@ -24,30 +24,12 @@ namespace Rename_file
                 string [] name = openFileDialog.FileNames;
                 string symbols = "", newSymbols = "";
 
-                while (symbols == "" || newSymbols == "")
-                {
-                    Console.Write("Введите символы, которые необходимо заменить: ");
-                    symbols = Console.ReadLine();
+                Symbols.Get_symbols(ref symbols, ref newSymbols, ref name);
 
-                    
-                    if (symbols == "" || !name[0].Contains(symbols))
-                    {
-                        Console.Clear();
-                        continue;
-                    }
-                    Console.Write("Введите символы, которые нужно вставить: ");
-                    newSymbols = Console.ReadLine();
-                    
-                    if (newSymbols == "")
-                    {
-                        Console.Clear();
-                        continue;
-                    }
-                }
                 for (int i = 0; i < name.Length; i++)
                 {
-                string newName = name[i].Replace(symbols, newSymbols);
-                File.Move(name[i], newName);
+                    string newName = name[i].Replace(symbols, newSymbols);
+                    File.Move(name[i], newName);
                 }
                 Console.Write("Готово!");
             }
---
+++ Rename_file/Symbols.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rename_file
+{
+    public static class Symbols
+    {
+        public static void Get_symbols(ref string symbols, ref string newSymbols, ref string[] name)
+        {
+            while (symbols == "" || newSymbols == "")
+            {
+                Console.Write("Введите символы, которые необходимо заменить: ");
+                symbols = Console.ReadLine();
+
+                if (symbols == "" || !name[0].Contains(symbols))
+                {
+                    Console.Clear();
+                    continue;
+                }
+                Console.Write("Введите символы, которые нужно вставить: ");
+                newSymbols = Console.ReadLine();
+
+                if (newSymbols == "")
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+    }
+}
